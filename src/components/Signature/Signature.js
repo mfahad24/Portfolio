bfab2a1dd9dd47bc155faacab09b8d83368a0498
Mohammed Fahad <mohammedfahad@live.com>
2019-12-01 08:20:37 -0500

    minor styling changes

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -3,7 +3,7 @@ import './Signature.css'
 
 function Signature() {
   return <div className="signaturecontainer">
-      <p>Built in <a className="detroit" href="https://www.youtube.com/watch?v=_Yhyp-_hX2s" target="_blank " rel="noopener noreferrer">Detroit</a> by Mohammed Fahad using React &#9679; 2019</p>
+      <p>Built in <a className="detroit" href="https://www.youtube.com/watch?v=_Yhyp-_hX2s" target="_blank " rel="noopener noreferrer">Detroit</a> by Mohammed Fahad using React</p>
   </div>
 }
 export default Signature;
